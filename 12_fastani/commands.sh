38b75620262f1fe395a24c284938d3ce4054f045
David Studholme <d.j.studholme@exeter.ac.uk>
2026-04-01 19:41:17 +0000

    Copied PP1, PP2, PP5 and NCPPB genomes into 01_genomes/

--- 12_fastani/commands.sh
+++ 12_fastani/commands.sh
@@ -1,6 +1,6 @@
 ### PRepare lists of query and reference genomes
-ls ../01_genomes/*brasiliense.fasta  ../01_genomes/*jejuense.fasta  ../08_unicycler/PRT5.unicycler.assembly.fasta > ref_list.txt
-ls ../01_genomes/*brasiliense.fasta  ../01_genomes/*jejuense.fasta  ../08_unicycler/PRT5.unicycler.assembly.fasta > query_list.txt
+ls ../01_genomes/*.fasta > ref_list.txt 
+ls ../01_genomes/*.fasta > query_list.txt
 
 ### FastANI has already been installed via Conda
 conda activate fastani_env
