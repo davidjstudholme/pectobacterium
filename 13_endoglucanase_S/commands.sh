8d37e65dda1ba848fdb48b09a07a6f68f724dfdf
David Studholme <d.j.studholme@exeter.ac.uk>
2026-06-07 08:46:50 +0000

    Ran Clinker on 7 genomes

--- 13_endoglucanase_S/commands.sh
+++ 13_endoglucanase_S/commands.sh
@@ -3,7 +3,10 @@
 
 python3 extract_gbk.py MHG0286677.1 MHG0286683.1
 python3 extract_gbk.py WP_010280900.1 WP_014914900.1
-
+python3 extract_gbk.py WP_010280900.1 WP_039500482.1
+python3 extract_gbk.py WP_039500492.1 WP_040034872.1
+python3 extract_gbk.py KHT20221.1 KHT20215.1
+python3 extract_gbk.py WP_010280900.1 WP_075278061.1
 
 conda activate clinker_env
 
@@ -11,7 +14,9 @@ conda list -n clinker_env > clinker_env_packages.txt
 conda env export > clinker_env.yaml
 
 
-clinker -p endoglucanase.clinker.html  DQ.WP_010280900.1_to_WP_014914900.1.gbk  NAK_239.WP_010280900.1_to_WP_014914900.1.gbk  PP1.3.MHG0286677.1_to_MHG0286683.1.gbk
+clinker -p endoglucanase.clinker.html  DQ.WP_010280900.1_to_WP_014914900.1.gbk  NAK_239.WP_010280900.1_to_WP_014914900.1.gbk  PP1.3.MHG0286677.1_to_MHG0286683.1.gbk SM.WP_010280900.1_to_WP_039500482.1.gbk BY2.WP_039500492.1_to_WP_040034872.1.gbk Y65.scaffold9.KHT20221.1_to_KHT20215.1.gbk BC1.WP_010280900.1_to_WP_075278061.1.gbk
+
+
 
 
 
