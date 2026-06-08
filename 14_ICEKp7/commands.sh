4f55155f6caa5300fbb4d49a7c1b2202805e23e8
David Studholme <d.j.studholme@exeter.ac.uk>
2026-06-08 20:53:29 +0000

    Ran clinker

--- 14_ICEKp7/commands.sh
+++ 14_ICEKp7/commands.sh
@@ -1,8 +1,7 @@
 
 python3 extract_gbk.py MHG0286805.1 MHG0286858.1
 
-       python3 extract_gbk.py WP_039504553.1 WP_075277962.1
-python3 extract_gbk.py  WP_406643116.1 WP_039498696.1
+python3 extract_gbk.py  WP_406643116.1 WP_039500299.1
 
 
 
