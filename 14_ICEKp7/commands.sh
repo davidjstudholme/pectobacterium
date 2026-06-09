33c4a8a4a705e526ae62684b94d078d484db5aa6
David Studholme <d.j.studholme@exeter.ac.uk>
2026-06-09 19:14:18 +0000

    Ran Clinker

--- 14_ICEKp7/commands.sh
+++ 14_ICEKp7/commands.sh
@@ -1,13 +1,10 @@
 
-python3 extract_gbk.py MBN3101359.1 MBN3101231.1
+python3 extract_gbk.py MBN3101275.1 MBN3101231.1
 python3 extract_gbk.py WP_015730138.1 MBN3183672.1
 python3 extract_gbk.py MBN3097640.1 MBN3097686.1
 python3 extract_gbk.py MBN3163597.1 MBN3163643.1
 python3 extract_gbk.py MFP9462767.1 MFP9462748.1
 python3 extract_gbk.py MHG0286806.1 MHG0286858.1
-
-
-
 
 conda activate clinker_env
 
